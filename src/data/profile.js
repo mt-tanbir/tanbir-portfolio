0ebafdb29303aaa08b12364eea256149ed423ecd
mt-tanbir <97947584+mt-tanbir@users.noreply.github.com>
2025-05-27 21:55:28 +0600

    Initial commit of my portfolio project

---
+++ src/data/profile.js
@@ -0,0 +1,14 @@
+const ProfileData = {
+  img: require("../assets/Images/hero.png"),
+  logo: require("../assets/Images/Name.png"),
+  name: "Tanbir Mahmud",
+  professions: ["Web Developer", "Free Lancer", "Enthusiastic Dev"],
+  info: [
+    "Knack of building web applications using MERN stack.",
+    "I enjoy designing tech websites.",
+  ],
+  resume:
+    "https://drive.google.com/file/d/1KZ74lcdexChQz93qfAhqic6NFIoQW6OY/view?usp=sharing",
+};
+
+export default ProfileData;
